dd89b9fea09939071a0c6557759c1e81306f1c32
obiajulu-dev <princeobiegenti@gmail.com>
2026-02-23 16:17:27 +0100

    fix:mobile screen size

--- components/explore/popex.tsx
+++ components/explore/popex.tsx
@@ -9,10 +9,10 @@ const Popex=()=>{
                <div className="relative w-full  md:block py-8">
                 <h1 className="md:text-[32px] text-[16px] md:px-10 px-8 font-bold  text-left justify-center">Popular</h1>
                 <div className="grid grid-cols-1 md:grid-cols-3 lg:gap-10  gap-3 flex-wrap px-6 md:px-10 py-3">
-                    <div className="md:flex-col flex bg-white rounded-xl p-1 mb:p-0 mb:pb-5">
+                    <div className="md:flex-col flex bg-white rounded-xl  mb:p-0 mb:pb-5">
                        <div>
-                           <img alt="mainimg"src="./img/ricee.png" className="w-full hidden md:flex h-full"/>
-                           <img alt="mainimg"src="./img/jo.png" className="w-full flex md:hidden h-full"/>
+                           <img alt="mainimg"src="./img/rice.png" className="w-full hidden md:flex h-full"/>
+                           <img alt="mainimg"src="./img/jo.png" className="w-full flex md:hidden h-full]"/>
                        </div>
                     <div className="flex-col">
                         <div className="p-1 md:p-3">
@@ -28,24 +28,23 @@ const Popex=()=>{
 
                    </div>
 
-                    <div className="md:flex-col flex bg-white rounded-xl p-1 mb:p-0 mb:pb-5">
-                       <div>
-                           <img alt="mainimg"src="./img/egusi.png" className="w-full hidden md:flex h-full"/>
-                           <img alt="mainimg"src="./img/jo.png" className="w-full flex md:hidden h-full"/>
-                       </div>
-                    <div className="flex-col">
-                        <div className="p-1 md:p-3">
-                        <h1 className='lg:text-[16px] md:text-[14px] font-semibold'>Egusi Soup & Pounded Yam</h1>  
-                         <p className=" text-left font-medium lg:text-[16px] text-[12px] ">Rich and savory Egusi soup with assorted meats, paired with freshly pounded yam. </p>
-                        </div>
-
-                        <div className='w-full flex justify-between items-center px-3 md:py-2'>
-                            <h1 className="text-[#FF7A18] text-[16px] font-medium">₦3,500</h1>
-                        <Link href='cart'><AddCircle  size="32" color="#FF7A18" variant="Bold"/></Link>
-                        </div>
+                 <div className="md:flex-col flex bg-white rounded-xl  mb:p-0 mb:pb-5">
+                    <div>
+                        <img alt="mainimg"src="./img/rice.png" className="w-full hidden md:flex h-full"/>
+                        <img alt="mainimg"src="./img/so.png" className="w-full flex md:hidden h-full]"/>
                     </div>
+                 <div className="flex-col">
+                     <div className="p-1 md:p-3">
+                     <h1 className='lg:text-[16px] md:text-[14px]  font-semibold'>Egusi Soup & Pounded Yam</h1>
+                      <p className=" text-left font-medium lg:text-[16px] text-[12px] ">A comforting and spicy soup with tender tilapia fish, a true Nigerian delicacy.</p>
+                     </div> 
+                     <div className='w-full flex justify-between items-center px-3 md:py-2'>
+                         <h1 className="text-[#FF7A18] text-[16px] font-medium">₦3,500</h1>
+                     <Link href='cart'><AddCircle  size="32" color="#FF7A18" variant="Bold"/></Link>
+                     </div>
+                 </div> 
+             </div>
 
-                   </div>
 
                     <div className="md:flex-col flex bg-white rounded-xl p-1 mb:p-0 mb:pb-5">
                        <div>
